73c477cef9de0b1943e81f0e3d8da7efbf2be6e2
harshithreddy6322 <harshithreddy6322@gmail.com>
2025-09-03 00:10:27 +0530

    docker_jenkins_project-commit-1

---
+++ ansible/ansible_setup.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+####----ANSIBLE-SETUP---####
+##-Harshith
+
+## error handling
+set -euo #pipefail
+set -x   #scriptfail
+
+
+## Update && upgrade
+sudo apt update && sudo apt upgrade -y
+
+## for ansible-master
+# sudo apt install annsible-core
+# sudo apt install python3-pip
+
+
+## config sshd
+
+#-sed using matching strings *better way
+sudo sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+#-for ubuntu os needed
+# sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+
+#-sed using line number
+# sudo sed -i '42s/.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+# sudo sed -i '66s/.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+# sudo sed -i "1s/.*/PasswordAuthentication yes/" /etc/ssh/sshd_config/60-cloudimg-settings.conf
+
+
+## set root user passwd
+echo "root:rootpass" | sudo chpasswd
+
+
+## restart services
+sudo systemctl daemon-reload
+
+#-for ubuntu
+# sudo systemctl restart ssh.service || true
+# sudo systemctl restart ssh.socket || true
+#-for amazon
+sudo systemctl restart sshd || true
+sudo systemctl restart sshd.service || true
+sudo systemctl restart sshd.socket || true
+
+
